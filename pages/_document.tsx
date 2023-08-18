fc07214fa6bb3b090fe10e604eb8b6214ef30b55
Kohta9521 <kohta0421kochi@gmail.com>
2023-08-18 20:19:43 +0900

    add nextjs setting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,12 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+    <Html lang="ja">
     </Html>
   )
 }
