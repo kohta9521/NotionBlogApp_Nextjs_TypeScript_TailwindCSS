f382afed62646d1b9871ac908eb3e865bc795055
Kohta9521 <kohta0421kochi@gmail.com>
2023-08-20 17:36:02 +0900

    add tailwindcss but not reflect

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-black'>
+      <h1 className='text-lg'>sample</h1>
     </div>
   )
 }
