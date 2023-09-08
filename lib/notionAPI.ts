7173c907ccd3c03b647369a51653ace2b9a18c86
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-08 09:26:10 +0900

    add notion database and notionqh library install

---
+++ lib/notionAPI.ts
@@ -0,0 +1,17 @@
+import {Client } from "@notionhq/client";
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+// クエリ関数
+export const getAllPosts = async() => {
+  const posts = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+    page_size: 100,
+  });
+
+  const allPosts = posts.results;
+
+  return allPosts;
+}
